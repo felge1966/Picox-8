786b51e834d8ea8b7dca4703b1026777c5da1632
Hans Hübner <hans.huebner@gmail.com>
2024-07-06 07:53:20 +0200

    refactor vhdl

--- cpld/picox-8.vhd
+++ cpld/picox-8.vhd
@@ -4,33 +4,33 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity PicoX8 is
-    Port (
-        clk       : in  std_logic;                   -- System clock
-        ioreq     : in  std_logic;                   -- I/O request signal
-        rd        : in  std_logic;                   -- Read signal
-        address   : in  std_logic_vector(7 downto 0);-- Address bus (8-bit for I/O)
-        data      : inout std_logic_vector(7 downto 0) -- Data bus (8-bit)
+  Port (
+    clk       : in  std_logic;                   -- System clock
+    ioreq     : in  std_logic;                   -- I/O request signal
+    rd        : in  std_logic;                   -- Read signal
+    address   : in  std_logic_vector(7 downto 0);-- Address bus (8-bit for I/O)
+    data      : inout std_logic_vector(7 downto 0) -- Data bus (8-bit)
     );
 end PicoX8;
 
 architecture Behavioral of PicoX8 is
-    constant FIXED_VALUE : std_logic_vector(7 downto 0) := "10101010"; -- Fixed value to return
-    signal data_out      : std_logic_vector(7 downto 0); -- Internal signal for data bus
-    signal oe            : std_logic; -- Output enable signal
+  constant SERIAL_STATUS : std_logic_vector(7 downto 0) := "00000001"; -- modem, carrier
+  constant IO_ADDRESS    : std_logic_vector(7 downto 0) := x"86";
+  signal data_out        : std_logic_vector(7 downto 0);
+  signal oe              : std_logic;
 begin
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if (ioreq = '0') and (rd = '0') and (address = "10000110") then -- I/O read at address 86h
-                data_out <= FIXED_VALUE;
-                oe <= '1';
-            else
-                oe <= '0';
-            end if;
-        end if;
-    end process;
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if (ioreq = '0') and (rd = '0') and (address = IO_ADDRESS) then
+        data_out <= SERIAL_STATUS;
+        oe <= '1';
+      else
+        oe <= '0';
+      end if;
+    end if;
+  end process;
 
-    -- Drive the data bus with the fixed value when output enable is active
-    data <= data_out when oe = '1' else (others => 'Z');
+  data <= data_out when oe = '1' else (others => 'Z');
 end Behavioral;
 
