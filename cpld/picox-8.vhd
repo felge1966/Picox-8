fdebfec0393edcd82288d009c9ae97f02b065950
Hans Hübner <hans.huebner@gmail.com>
2024-07-07 17:51:23 +0200

    serial port access with DAK Comm works

--- cpld/picox-8.vhd
+++ cpld/picox-8.vhd
@@ -5,6 +5,10 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity PicoX8 is
   Port (
+    led0      : out std_logic;
+    led1      : out std_logic;
+    rx_n      : out std_logic;
+    rx        : in std_logic;
     clk       : in  std_logic;                   -- System clock
     ioreq_n   : in  std_logic;                   -- I/O request signal
     rd_n      : in  std_logic;                   -- Read signal
@@ -18,6 +22,7 @@ architecture Behavioral of PicoX8 is
   constant IO_ADDRESS    : std_logic_vector(7 downto 0) := x"86";
   signal data_out        : std_logic_vector(7 downto 0);
   signal oe              : std_logic;
+  signal accessed        : std_logic := '0';
 begin
   process(clk)
   begin
@@ -25,6 +30,7 @@ begin
       if (ioreq_n = '0') and (rd_n = '0') and (address = IO_ADDRESS) then
         data_out <= SERIAL_STATUS;
         oe <= '1';
+        accessed <= '1';
       else
         oe <= '0';
       end if;
@@ -32,5 +38,12 @@ begin
   end process;
 
   data <= data_out when oe = '1' else (others => 'Z');
+
+  led0 <= '0';
+  led1 <= not accessed;
+
+  -- RS232 RX on expansion bus needs to be inverted
+  rx_n <= not rx;
+
 end Behavioral;
 
