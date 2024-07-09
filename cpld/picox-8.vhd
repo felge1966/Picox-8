e58acb92fd49610b1992917f66bf03c6e007f4ed
Hans Hübner <hans.huebner@gmail.com>
2024-07-09 10:52:25 +0200

    refactor

--- cpld/picox-8.vhd
+++ cpld/picox-8.vhd
@@ -41,22 +41,27 @@ entity PicoX8 is
 end PicoX8;
 
 architecture Behavioral of PicoX8 is
-  constant ADDR_TONE_DIALER     : std_logic_vector(7 downto 0) := x"84"; -- 0
-  constant ADDR_MODEM_CONTROL   : std_logic_vector(7 downto 0) := x"85"; -- 1
-  constant ADDR_MODEM_STATUS    : std_logic_vector(7 downto 0) := x"86"; -- 2
-  constant ADDR_RAMDISK_DATA    : std_logic_vector(7 downto 0) := x"80"; -- 3
-  constant ADDR_RAMDISK_CONTROL : std_logic_vector(7 downto 0) := x"81"; -- 4
-  signal modem_tone_dialer      : std_logic_vector(7 downto 0);
-  signal modem_control          : std_logic_vector(7 downto 0);
-  signal modem_status           : std_logic_vector(7 downto 0);
-  signal ramdisk_data           : std_logic_vector(7 downto 0);
-  signal ramdisk_command        : std_logic_vector(7 downto 0);
-  signal ramdisk_ibf            : std_logic;
-  signal ramdisk_obf            : std_logic;
-  signal data_out               : std_logic_vector(7 downto 0);
-  signal oe                     : std_logic;
-  signal pico_data_out          : std_logic_vector(7 downto 0);
-  signal pico_oe                : std_logic;
+  constant ADDR_TONE_DIALER      : std_logic_vector(7 downto 0) := x"84";
+  constant ADDR_MODEM_CONTROL    : std_logic_vector(7 downto 0) := x"85";
+  constant ADDR_MODEM_STATUS     : std_logic_vector(7 downto 0) := x"86";
+  constant ADDR_RAMDISK_DATA     : std_logic_vector(7 downto 0) := x"80";
+  constant ADDR_RAMDISK_CONTROL  : std_logic_vector(7 downto 0) := x"81";
+  constant INDEX_TONE_DIALER     : std_logic_vector(2 downto 0) := "000";
+  constant INDEX_MODEM_CONTROL   : std_logic_vector(2 downto 0) := "001";
+  constant INDEX_MODEM_STATUS    : std_logic_vector(2 downto 0) := "010";
+  constant INDEX_RAMDISK_DATA    : std_logic_vector(2 downto 0) := "011";
+  constant INDEX_RAMDISK_CONTROL : std_logic_vector(2 downto 0) := "100";
+  signal modem_tone_dialer       : std_logic_vector(7 downto 0);
+  signal modem_control           : std_logic_vector(7 downto 0);
+  signal modem_status            : std_logic_vector(7 downto 0);
+  signal ramdisk_data            : std_logic_vector(7 downto 0);
+  signal ramdisk_command         : std_logic_vector(7 downto 0);
+  signal ramdisk_ibf             : std_logic;
+  signal ramdisk_obf             : std_logic;
+  signal data_out                : std_logic_vector(7 downto 0);
+  signal oe                      : std_logic;
+  signal pico_data_out           : std_logic_vector(7 downto 0);
+  signal pico_oe                 : std_logic;
 begin
   process(clk)
   begin
@@ -73,66 +78,74 @@ begin
         irq_modem_control   <= '0';
         irq_ramdisk_command <= '0';
       else
+        -- Handle access from the Z80 side
         if (ioreq_n = '0') then
           oe <= '0';
           if (rd_n = '0') then
             oe <= '1';
-            if (address = ADDR_MODEM_STATUS) then
-              data_out <= modem_status;
-            elsif (address = ADDR_RAMDISK_DATA) then
-              data_out <= ramdisk_data;
-              ramdisk_ibf <= '0';
-            elsif (address = ADDR_RAMDISK_CONTROL) then
-              data_out <= (0 => ramdisk_ibf, 1 => ramdisk_obf, others => '0');
-            else
-              data_out <= x"00";
-            end if;
+            case address is
+              when ADDR_MODEM_STATUS =>
+                data_out <= modem_status;
+              when ADDR_RAMDISK_DATA =>
+                data_out <= ramdisk_data;
+                ramdisk_ibf <= '0';
+              when ADDR_RAMDISK_CONTROL =>
+                data_out <= (0 => ramdisk_ibf, 1 => ramdisk_obf, others => '0');
+              when others =>
+                data_out <= x"00";
+            end case;
           elsif (wr_n = '0') then
-            if (address = ADDR_TONE_DIALER) then
-              modem_tone_dialer <= data;
-              irq_tone_dialer <= '1';
-            elsif (address = ADDR_MODEM_CONTROL) then
-              modem_control <= data;
-              irq_modem_control <= '1';
-            elsif (address = ADDR_RAMDISK_DATA) then
-              ramdisk_data <= data;
-              ramdisk_obf <= '1';
-            elsif (address = ADDR_RAMDISK_CONTROL) then
-              ramdisk_command <= data;
-              irq_ramdisk_command <= '1';
-            end if;
+            case address is
+              when ADDR_TONE_DIALER =>
+                modem_tone_dialer <= data;
+                irq_tone_dialer <= '1';
+              when ADDR_MODEM_CONTROL =>
+                modem_control <= data;
+                irq_modem_control <= '1';
+              when ADDR_RAMDISK_DATA =>
+                ramdisk_data <= data;
+                ramdisk_obf <= '1';
+              when ADDR_RAMDISK_CONTROL =>
+                ramdisk_command <= data;
+                irq_ramdisk_command <= '1';
+              when others =>
+                null;
+            end case;
           end if;
         end if;
-
+        -- Handle access from the Pico side
         if (pico_stb = '1') then
           pico_oe <= '0';
 
-          if pico_dir = '0' and pico_addr = x"0" then
-            pico_data_out <= modem_tone_dialer;
+          if (pico_dir = '0') then
             pico_oe <= '1';
-            irq_tone_dialer <= '0';
-
-          elsif pico_dir = '0' and pico_addr = x"1" then
-            pico_data_out <= modem_control;
-            pico_oe <= '1';
-            irq_modem_control <= '0';
-
-          elsif pico_dir = '1' and pico_addr = x"2" then
-            modem_status <= pico_data;
-
-          elsif pico_dir = '0' and pico_addr = x"3" then
-            pico_data_out <= ramdisk_data;
-            pico_oe <= '1';
-            ramdisk_obf <= '0';
-          elsif pico_dir = '1' and pico_addr = x"3" then
-            ramdisk_data <= pico_data;
-            ramdisk_ibf <= '1';
-
-          elsif pico_dir = '0' and pico_addr = x"4" then
-            pico_data_out <= ramdisk_command;
-            pico_oe <= '1';
-            irq_ramdisk_command <= '0';
-            ramdisk_obf <= '0';             -- flush output buffer
+            case pico_addr is
+              when INDEX_TONE_DIALER =>
+                pico_data_out <= modem_tone_dialer;
+                irq_tone_dialer <= '0';
+              when INDEX_MODEM_CONTROL =>
+                pico_data_out <= modem_control;
+                irq_modem_control <= '0';
+              when INDEX_RAMDISK_DATA =>
+                pico_data_out <= ramdisk_data;
+                ramdisk_obf <= '0';
+              when INDEX_RAMDISK_CONTROL =>
+                pico_data_out <= ramdisk_command;
+                irq_ramdisk_command <= '0';
+                ramdisk_obf <= '0';             -- flush output buffer
+              when others =>
+                pico_data_out <= x"00";
+            end case;
+          else
+            case pico_addr is
+              when INDEX_MODEM_STATUS =>
+                modem_status <= pico_data;
+              when INDEX_RAMDISK_CONTROL =>
+                ramdisk_data <= pico_data;
+                ramdisk_ibf <= '1';
+              when others =>
+                null;
+            end case;
           end if;
         end if;
       end if;
